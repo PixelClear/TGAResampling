6c5246ce54f0f169a0816976088033d406f5701b
PixelClear <parkul24@gmail.com>
2018-05-13 10:09:41 +0200

    Implemented bilinear filtering and cleaned up code

--- TGAResample/TGA.h
+++ TGAResample/TGA.h
@@ -2,12 +2,21 @@
 #include <fstream>
 #include <vector>
 #include <string>
-
-#define CLAMP(v, min, max) if (v < min) { v = min; } else if (v > max) { v = max; } 
+#include <cassert>
+#include <memory>
 
 class Tga
 {
-private:
+
+public:
+	Tga() = default;
+	~Tga() {};
+
+	bool loadTGA(const char* FilePath);
+	bool saveTGA(const char* fileName);
+	void resizeTGA(Tga& destTga);
+
+public:
 	std::vector<std::uint8_t> pixels_;
 	bool bCompressed_;
 	std::uint32_t width_;
@@ -20,15 +29,57 @@ private:
 	std::vector<std::uint8_t> imageData_;
 	const std::uint8_t compressed_[12] = { 0x0, 0x0, 0xA, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 };
 
-public:
-	Tga() = default;
-	~Tga() {};
+};
 
-	bool loadTGA(const char* FilePath);
-	bool saveTGA(const char* fileName);
-	Tga& resizeTGA();
-	std::vector<std::uint8_t> getPixels() const { return pixels_; }
-	std::uint32_t getWidth() const { return this->width_; }
-	std::uint32_t getHeight() const { return this->height_; }
-	bool hasAlphaChannel() const { return bitsPerPixel_ == 32; }
+
+class Resample
+{
+public:
+	Resample() = default;
+	~Resample() {}
+
+public:
+	virtual void resize(Tga& in, Tga& out) = 0;
+};
+
+class NearestNeighbour : public Resample
+{
+public:
+	NearestNeighbour() = default;
+	virtual ~NearestNeighbour() {}
+public:
+
+	void resize(Tga& in, Tga& out) override;
+};
+
+class Bilinear : public Resample
+{
+public:
+	Bilinear() = default;
+	virtual ~Bilinear() {}
+public:
+
+	void resize(Tga& in, Tga& out) override;
+};
+
+namespace Helper
+{
+	template<class T>
+	inline void clamp(T& v, const T min, const T max) {
+		if (v < min) { v = min; }
+		else if (v > max) { v = max; }
+	}
+
+	inline float lerp(const float a, const float b, const float t)
+	{
+		return a * (1.0f - t) + b * t;
+	}
+
+	inline uint8_t* getClampedSample(Tga& in, int x, int y)
+	{
+		clamp<int>(x, 0, in.width_ - 1);
+		clamp<int>(y, 0, in.height_ - 1);
+
+		return &in.imageData_[(y * in.pitch_) + (x + 0) * in.numOfChannels_];
+	}
 };
--- TGAResample/main.cpp
+++ TGAResample/main.cpp
@@ -1,6 +1,74 @@
 
 #include "TGA.h"
 
+void NearestNeighbour::resize(Tga& in, Tga& out)
+{
+	for (uint32_t j = 0; j < out.height_; j++)
+	{
+		float v = static_cast<float>(j) / static_cast<float>(out.height_ - 1);
+		for (uint32_t i = 0; i < out.width_; i++)
+		{
+			float u = static_cast<float>(i) / static_cast<float>(out.width_ - 1);
+
+			int x = int(u * in.width_);
+			int y = int(v * in.height_);
+			
+			assert(in.numOfChannels_ == out.numOfChannels_);
+
+		    auto pixel = &out.imageData_[(j * out.pitch_) + (i + 0) * in.numOfChannels_];
+			auto sample = Helper::getClampedSample(in, x, y);
+
+			pixel[0] = sample[0];
+			pixel[1] = sample[1];
+			pixel[2] = sample[2];
+		}
+	}
+}
+
+void Bilinear::resize(Tga& in, Tga& out)
+{
+	for (uint32_t j = 0; j < out.height_; j++)
+	{
+		float v = static_cast<float>(j) / static_cast<float>(out.height_ - 1);
+		for (uint32_t i = 0; i < out.width_; i++)
+		{
+			float u = static_cast<float>(i) / static_cast<float>(out.width_ - 1);
+
+			float x = (u * in.width_) - 0.5f;
+			int xint = int(x);
+			float tx = x - floor(x);
+
+			float y = (v * in.height_) - 0.5f;
+			int yint = int(y);
+			float ty = y - floor(y);
+
+			auto sample00 = Helper::getClampedSample(in, xint, yint);
+			auto sample10 = Helper::getClampedSample(in, xint + 1, yint);
+			auto sample01 = Helper::getClampedSample(in, xint, yint + 1);
+			auto sample11 = Helper::getClampedSample(in, xint + 1, yint + 1);
+
+			uint8_t finaleSample[3];
+			for (uint32_t i = 0 ; i < 3; i++)
+			{
+				//Interpolation in X direction
+				float col0 = Helper::lerp(sample00[i], sample01[i], tx);
+				float col1 = Helper::lerp(sample10[i], sample11[i], tx);
+				//Interpolation in Y direction
+				float col2 = Helper::lerp(col0, col1, ty);
+				Helper::clamp<float>(col2, 0.0f, 255.0f);
+				finaleSample[i] = static_cast<uint8_t>(col2);
+			}
+
+			//Get Current pixel
+			auto pixel = &out.imageData_[(j * out.pitch_) + (i + 0) * in.numOfChannels_];
+
+			pixel[0] = finaleSample[0];
+			pixel[1] = finaleSample[1];
+			pixel[2] = finaleSample[2];
+		}
+	}
+}
+
 bool Tga::loadTGA(const char* FilePath)
 {
 	std::fstream in{ FilePath, std::ios::in | std::ios::binary };
@@ -17,7 +85,7 @@ bool Tga::loadTGA(const char* FilePath)
 		size_ = ((width_ * bitsPerPixel_ + 31) / 32) * 4 * height_ ;
 		numOfChannels_ = bitsPerPixel_ / 8;
 		pitch_ = width_ * numOfChannels_;
-		if (width_ <= 0 || height_ <=0 || (bitsPerPixel_ != 24) && (bitsPerPixel_ != 32))
+		if (width_ <= 0 || height_ <= 0 || (bitsPerPixel_ != 24) && (bitsPerPixel_ != 32))
 		{
 			in.close();
 			return false;
@@ -65,9 +133,8 @@ bool Tga::saveTGA(const char* fileName)
 	return true;
 }
 
-Tga& Tga::resizeTGA()
+void Tga::resizeTGA(Tga& destTga) //New name is assumed with .tga extension
 {
-	Tga destTga{};
 	destTga.width_ = static_cast<uint32_t>(width_ / 2);
 	destTga.height_ = static_cast<uint32_t>(height_ / 2);
 	destTga.bitsPerPixel_ = bitsPerPixel_;
@@ -76,38 +143,17 @@ Tga& Tga::resizeTGA()
 	destTga.pitch_ = destTga.width_ * destTga.numOfChannels_;
 	destTga.imageData_.resize(destTga.size_);
 
-	for (uint32_t j = 0; j < destTga.height_; j++)
-	{
-		float v = static_cast<float>(j) / static_cast<float>(destTga.height_ - 1);
-		int rows = 0;
-		for (uint32_t i = 0; i < destTga.width_; i++)
-		{
-			float u = static_cast<float>(i) / static_cast<float>(destTga.width_ - 1);
-
-			int x = int(u * width_);
-			int y = int(v * height_);
-
-			CLAMP(x, 0, width_ - 1);
-			CLAMP(y, 0, height_ - 1);
-			
-			uint8_t* pixel = &destTga.imageData_[(j * destTga.pitch_) + (i + 0) * numOfChannels_];
-			uint8_t* sample = &imageData_[(y * pitch_) + (x + 0) * numOfChannels_];
-
-			pixel[0] = sample[0];
-			pixel[1] = sample[1];
-			pixel[2] = sample[2];
-		}
-	}
-
-	destTga.saveTGA("Resized.tga");
-	return destTga;
+	std::unique_ptr<Resample> sampler = std::make_unique<Bilinear>();
+	sampler->resize(*this, destTga);
+	
+	sampler.reset(nullptr);
 }
 
 int main()
 {
 	Tga tga;
-	//tga.loadTGA("xing_b32.tga");
+	Tga out;
 	tga.loadTGA("MARBLES.tga");
-	tga.saveTGA("test.tga");
-	tga.resizeTGA();
+	tga.resizeTGA(out);
+	out.saveTGA("Resize.tga");
 }
